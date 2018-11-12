abc3d29c79a772b02d0695e5ac4ef1b0e55b5f37
Molly McCarron <mccarronmollye@gmail.com>
2018-11-12 15:29:02 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,8 +20,6 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
 end
 
@@ -32,6 +30,9 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+  shipping_manifest["muskets"] => 2
+  shipping_manifest["gun powder"] => 4
+  puts shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
 
